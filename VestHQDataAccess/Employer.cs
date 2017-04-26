d0a39e51283717d2649b078c3c677c250c6732b8
David Giard <djgiard@hotmail.com>
2017-04-25 19:30:42 -0500

    Add DataAcccess project; Add Model properties

---
+++ VestHQDataAccess/Employer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VestHQDataModels;
+
+namespace VestHQDataAccess
+{
+    public class Employer
+    {
+        public IEnumerable<Employer> GetAllEmployers()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
--- VestHQDataModels/StockPriceHistory.cs
+++ VestHQDataModels/StockPriceHistory.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VestHQDataModels
 {
     public class StockPriceHistory
     {
+        [Key()]
         public string Id { get; set; }
+
+        [ForeignKey("Stock")]
+        public string StockId { get; set; }
+
         public DateTime Time { get; set; }
 
         public string Ticker { get; set; }
