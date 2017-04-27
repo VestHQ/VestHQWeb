fe3c47d6b00f1179ddfedd862a099e4eb2378001
David Giard <djgiard@hotmail.com>
2017-04-27 15:52:32 -0500

    Remove DAL project

--- VestHQBusinessLib/FundPriceHistoryLib.cs
+++ VestHQBusinessLib/FundPriceHistoryLib.cs
@@ -21,11 +21,6 @@ namespace VestHQBusinessLib
             fundList = await FundDataAccess.GetAllFunds();
             foreach (Fund fund in fundList)
                 await RefreshCurrentFundPrices(fund.Ticker, fund.Id);
-
-            // TODO: Get funds from fund table
-            //await RefreshCurrentFundPrices("VTI", "1823329208");
-            //await RefreshCurrentFundPrices("VOO", "1238292754");
-            //await RefreshCurrentFundPrices("MGC", "674192873");
         }
 
         // Refreshes a single fund price
