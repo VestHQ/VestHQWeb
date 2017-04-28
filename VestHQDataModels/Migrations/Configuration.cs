ed956668545f5a1545c79d7ce647501473a9cb63
giannich <giannich@uchicago.edu>
2017-04-28 14:58:07 -0500

    Seeded SurveyQuestions and SurveyAnswers

--- VestHQDataModels/Migrations/Configuration.cs
+++ VestHQDataModels/Migrations/Configuration.cs
@@ -40,7 +40,6 @@ namespace VestHQDataModels.Migrations
                 new Employee {Id="00001", EmployerId="001", FirstName = "2Gianni", LastName = "Chen" },
                 new Employee {Id="00002", EmployerId="001", FirstName = "2David", LastName = "Giard" }
             };
-
             employees.ForEach(s => context.Employees.AddOrUpdate(p => p.LastName, s));
             context.SaveChanges();
 
@@ -50,8 +49,60 @@ namespace VestHQDataModels.Migrations
                 new Fund {Id="00002",Name="Vanguard2 S&P 500 ETF", Ticker="VOO",  },
                 new Fund {Id="00003",Name="Vanguard2 Mega Cap ETF", Ticker="MGC" }
             };
-
             funds.ForEach(s => context.Funds.AddOrUpdate(p => p.Ticker, s));
+            context.SaveChanges();
+
+            var surveyQuestions = new List<SurveyQuestion>
+            {
+                new SurveyQuestion {Text="How confident do you feel about retirement?", Order=1, Id="001"},
+                new SurveyQuestion {Text="Adding up all your accounts (cash, savings/checking accounts, mutual funds, stocks, etc.), what is the total value of your money?", Order=2, Id="002"},
+                new SurveyQuestion {Text="If the market were to go into another recession over the next decade, how would you expect your 401(k) to turn out?", Order=3, Id="003"},
+                new SurveyQuestion {Text="If a friend or family member described you, they would say you are:", Order=4, Id="004"},
+                new SurveyQuestion {Text="Imagine you invested $1000 over five years. Which outcome would you rather see?", Order=5, Id="005"},
+                new SurveyQuestion {Text="Would you rather have a stable job for the rest of your career and keep your current salary, or would you rather get a new job that increases your salary 10% a year but you don’t know how long you’ll be able to keep that job?", Order=6, Id="006"}
+            };
+
+            surveyQuestions.ForEach(s => context.SurveyQuestions.AddOrUpdate(p => p.Text, s));
+            context.SaveChanges();
+
+            var surveyAnswers = new List<SurveyAnswer>
+            {
+                new SurveyAnswer {Text="Confident", SurveyQuestionId="001", Order=1, Id="001"},
+                new SurveyAnswer {Text="Comfortable", SurveyQuestionId="001", Order=2, Id="002"},
+                new SurveyAnswer {Text="Neutral", SurveyQuestionId="001", Order=3, Id="003"},
+                new SurveyAnswer {Text="Worried", SurveyQuestionId="001", Order=4, Id="004"},
+                new SurveyAnswer {Text="Panicked", SurveyQuestionId="001", Order=5, Id="005"},
+
+
+                new SurveyAnswer {Text="less than $5,000", SurveyQuestionId="002", Order=1, Id="006"},
+                new SurveyAnswer {Text="$5,000 - $10,000", SurveyQuestionId="002", Order=2, Id="007"},
+                new SurveyAnswer {Text="$10,000 - $50,000", SurveyQuestionId="002", Order=3, Id="008"},
+                new SurveyAnswer {Text="$50,000 - $100,000", SurveyQuestionId="002", Order=4, Id="009"},
+                new SurveyAnswer {Text="$100,000 - $250,000", SurveyQuestionId="002", Order=5, Id="010"},
+                new SurveyAnswer {Text="$250,000+", SurveyQuestionId="002", Order=5, Id="011"},
+
+                new SurveyAnswer {Text="Lose a lot of money", SurveyQuestionId="003", Order=1, Id="012"},
+                new SurveyAnswer {Text="Lose a little money", SurveyQuestionId="003", Order=2, Id="013"},
+                new SurveyAnswer {Text="Keep around the same levels", SurveyQuestionId="003", Order=3, Id="014"},
+                new SurveyAnswer {Text="Make a little more than deposited", SurveyQuestionId="003", Order=4, Id="015"},
+                new SurveyAnswer {Text="Make more than the market", SurveyQuestionId="003", Order=5, Id="016"},
+
+                new SurveyAnswer {Text="Pretty cautious", SurveyQuestionId="004", Order=1, Id="017"},
+                new SurveyAnswer {Text="In the middle", SurveyQuestionId="004", Order=2, Id="018"},
+                new SurveyAnswer {Text="A risk-taker", SurveyQuestionId="004", Order=3, Id="019"},
+          
+                new SurveyAnswer {Text="Have a chance to grow or lose 12%, ending with either $1762 or $527", SurveyQuestionId="005", Order=1, Id="020"},
+                new SurveyAnswer {Text="Have a chance to grow or lose 8%, ending with either $1470 or $660", SurveyQuestionId="005", Order=2, Id="021"},
+                new SurveyAnswer {Text="Have a chance to grow or lose 2%, ending with either $1105 or $904", SurveyQuestionId="005", Order=3, Id="022"},
+
+                new SurveyAnswer {Text="I would for sure pick the safe job with the same salary", SurveyQuestionId="006", Order=1, Id="023"},
+                new SurveyAnswer {Text="I would probably pick the safe job with the same salary", SurveyQuestionId="006", Order=2, Id="024"},
+                new SurveyAnswer {Text="Not sure", SurveyQuestionId="006", Order=3, Id="025"},
+                new SurveyAnswer {Text="I would probably pick the 10% salary increase but less stable job", SurveyQuestionId="006", Order=4, Id="026"},
+                new SurveyAnswer {Text="I would for sure pick the 10% salary increase but less stable job", SurveyQuestionId="006", Order=5, Id="027"},
+            };
+
+            surveyAnswers.ForEach(s => context.SurveyAnswers.AddOrUpdate(p => p.Text, s));
             context.SaveChanges();
 
             /*
